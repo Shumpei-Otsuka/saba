9a40acf8ae854bbcf72de422ea44ff1c5c66c5cc
pen <fushimi.chiba@gmail.com>
2022-05-15 15:43:22 +0900

    (MainActivity.kt)Databaseからデータ取得・取得したデータからスピナーを表示する関数を追加。

--- app/src/main/java/com/so/saba/MainActivity.kt
+++ app/src/main/java/com/so/saba/MainActivity.kt
@@ -9,6 +9,10 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.io.Serializable
 import android.content.res.AssetManager
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
 import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
 
@@ -18,7 +22,7 @@ const val TRAIN_SCHEDULE_CONFIG = "com.so.saba.TRAIN_SCHEDULE_CONFIG"
 
 private val TAG: String = MainActivity::class.java.simpleName
 
-class MainActivity : AppCompatActivity() ,CoroutineScope{
+class MainActivity : AppCompatActivity(), CoroutineScope{
     override val coroutineContext: CoroutineContext //DBアクセス用にコルーチンの準備
         get() = Dispatchers.Main
 
@@ -36,17 +40,53 @@ class MainActivity : AppCompatActivity() ,CoroutineScope{
             }
         }
         //--DB用の初期化処理--
+
+
+        /** Called when the user taps the button */
+        //xml上ではDB処理用の関数を呼べないため、Activity(kotlin)側でonClickをoverride
+        findViewById<Button>(R.id.stationButton).setOnClickListener(object : View.OnClickListener {
+            //駅名から路線を検索をするボタンの処理
+            override fun onClick(v: View) {
+                launch{
+                    updateLineSpinnerList(db)
+                }
+            }
+        })
+        findViewById<Button>(R.id.directionButton).setOnClickListener(object : View.OnClickListener {
+            //駅名・路線から路線を検索をするボタンの処理
+            override fun onClick(v: View) {
+                launch{
+                    updateDestinationSpinnerList(db)
+                }
+            }
+        })
+        findViewById<Button>(R.id.registButton).setOnClickListener(object : View.OnClickListener {
+            //csv名をDatabaseから検索し、次ページへの遷移するボタンの処理
+            override fun onClick(v: View) {
+                launch{
+                    sendMessage(db)
+                }
+            }
+        })
     }
 
-    /** Called when the user taps the button */
-    fun sendMessage(view: View) {
+    fun sendMessage(db:AppDatabase) {
         Log.d(TAG, "sendMessage called.")
         // TODO: Example, must be replace
-        val station = "東京"
-        val line = "東海道線"
-        val destination = "大阪"
-        val weekdayPath = "exampleTrainScheduleWeekday.csv"
-        val holidayPath = "exampleTrainScheduleHoliday.csv"
+        //入力されている駅名・路線・方向を取得
+        val station = findViewById<EditText>(R.id.registeredStation).text.toString()
+        val line = findViewById<Spinner>(R.id.routespinner).selectedItem.toString()
+        val destination = findViewById<Spinner>(R.id.destinationSpinner).selectedItem.toString()
+
+        //csv名をDatabaseより取得
+        var weekdayPath = "exampleTrainScheduleWeekday.csv"
+        var holidayPath = "exampleTrainScheduleHoliday.csv"
+        var csvNamePair = listOf<TrainScheduleConfig>()
+        launch{
+            csvNamePair = getCsvNamePair(db)
+            weekdayPath = csvNamePair[0].weekdayPath
+            holidayPath = csvNamePair[0].holidayPath
+        }
         val trainScheduleConfig = TrainScheduleConfig(station, line, destination, weekdayPath, holidayPath)
         val intent = Intent(this, DisplayTrainScheduleActivity::class.java).apply {
             putExtra(TRAIN_SCHEDULE_CONFIG, trainScheduleConfig)
@@ -54,10 +94,44 @@ class MainActivity : AppCompatActivity() ,CoroutineScope{
         //intent.putExtra(EXTRA_MESSAGE, message) // Example
         startActivity(intent)
     }
+
+    suspend fun updateLineSpinnerList(db:AppDatabase){
+        val stationName = findViewById<EditText>(R.id.registeredStation).text.toString()
+        var routeList:List<String> = getLineFromDB(stationName, db)
+        val routeSpinner = findViewById<Spinner>(R.id.routespinner)
+        var adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item,
+            routeList
+        )
+        runOnUiThread(java.lang.Runnable {routeSpinner.adapter = adapter})
+    }
+    suspend fun updateDestinationSpinnerList(db:AppDatabase){
+        val stationName = findViewById<EditText>(R.id.registeredStation).text.toString()
+        val lineName = findViewById<Spinner>(R.id.routespinner).selectedItem.toString()
+        var destinationList = getDestinationFromDB(stationName, lineName, db)
+        val destinationSpinner = findViewById<Spinner>(R.id.destinationSpinner)
+        var adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item,
+            destinationList
+        )
+        runOnUiThread(java.lang.Runnable {destinationSpinner.adapter = adapter})
+    }
+
+    suspend fun getCsvNamePair(db:AppDatabase): List<TrainScheduleConfig> {
+        val stationName = findViewById<EditText>(R.id.registeredStation).text.toString()
+        val lineName = findViewById<Spinner>(R.id.routespinner).selectedItem.toString()
+        val destinationSpinner = findViewById<Spinner>(R.id.destinationSpinner).selectedItem.toString()
+        val csvNamePair = getCsvNameFromDB(stationName,lineName,destinationSpinner,db)
+        return csvNamePair
+    }
+
+
     private fun inputTrainScheduleConfigFile():MutableList<TrainScheduleConfig>{
         //TrainScheduleConfigのデータをアセットのファイルから読み込んでリストにして返す
         val assetManager: AssetManager = resources.assets
-        val fileInputStream  = assetManager.open("exampleTrainScheduleConfig.csv")
+        val fileInputStream  = assetManager.open("TrainScheduleConfig.csv")
         val reader = BufferedReader(InputStreamReader(fileInputStream, "UTF-8"))
         reader.readLine()
         var lineBuffer = ""
@@ -102,7 +176,7 @@ class MainActivity : AppCompatActivity() ,CoroutineScope{
         }
     }
 
-    private suspend fun getRouteFromDB(stationName :String , db :AppDatabase): List<String> {
+    private suspend fun getLineFromDB(stationName :String , db :AppDatabase): List<String> {
         val returnList : List<String>
         withContext(Dispatchers.IO){
             returnList = db.TimeScheduleTableDao().getLineByStation(stationName)
@@ -118,10 +192,10 @@ class MainActivity : AppCompatActivity() ,CoroutineScope{
         return returnList
     }
 
-    private suspend fun getCsvNameFromDB(stationName :String , routeName :String , DestinationName :String , db :AppDatabase): List<String> {
-        val returnList : List<String>
+    private suspend fun getCsvNameFromDB(stationName :String , lineName :String , DestinationName :String , db :AppDatabase): List<TrainScheduleConfig> {
+        val returnList : List<TrainScheduleConfig>
         withContext(Dispatchers.IO){
-            returnList = db.TimeScheduleTableDao().getCsvnameByInfo(stationName,routeName,DestinationName)
+            returnList = db.TimeScheduleTableDao().getCsvnameByInfo(stationName,lineName,DestinationName)
         }
         return returnList
     }
