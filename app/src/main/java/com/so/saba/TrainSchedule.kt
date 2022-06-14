6bc55ebbd987e5450a7497b5425f9b21a179eb5f
Shumpei Otsuka <1995612@gmail.com>
2022-06-14 19:03:07 +0900

    Add TrainScheduleConfigs data class. array of TrainScheduleConfig.
    Signed-off-by: Shumpei Otsuka <1995612@gmail.com>

--- app/src/main/java/com/so/saba/TrainSchedule.kt
+++ app/src/main/java/com/so/saba/TrainSchedule.kt
@@ -16,6 +16,25 @@ data class TrainScheduleConfig(
     var holidayPath: String = ""
 ): Serializable
 
+data class TrainScheduleConfigs(
+    var value: Array<TrainScheduleConfig>
+    ) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as TrainScheduleConfigs
+
+        if (!value.contentEquals(other.value)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return value.contentHashCode()
+    }
+}
+
 data class TrainData(
     val hour: Int,
     val minute: Int,
